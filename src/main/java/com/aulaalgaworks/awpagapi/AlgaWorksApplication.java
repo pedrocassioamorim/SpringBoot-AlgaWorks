e11e74e552edb258027b0326cf4818649bb2a1c7
Pedro Amorim <amorimpedrocassio@gmail.com>
2024-03-18 16:52:34 -0300

    preparando ambiente

---
+++ src/main/java/com/aulaalgaworks/awpagapi/AlgaWorksApplication.java
@@ -0,0 +1,13 @@
+package com.aulaalgaworks.awpagapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgaWorksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgaWorksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aulaalgaworks/awpagapi/AlgaWorksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aulaalgaworks.awpagapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgaWorksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
